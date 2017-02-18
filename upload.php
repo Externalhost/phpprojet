20b02709a62ed0ddb1c576bfb42bf10101efef67
Newolnyx <lowen.coldefy@ynov.com>
2017-02-18 02:07:39 +0100

    mise à jour upload

--- upload.php
+++ upload.php
@@ -1,4 +1,21 @@
 <?php
+session_start();
+require_once('class/user.php');
+if(!empty($_SESSION)){
+
+$iduser = $_SESSION['id'];
+
+}else{
+    $iduser = NULL;
+}
+
+$uploaddb = new user;
+$max = 25;
+
+
+// L'utilisateur a envoyé l'image
+
+
 if (!empty($_FILES)) {
     $mime_valid = ['image/png', 'image/jpeg','image/gif'];
     $extension_valid = ['png', 'jpeg','jpg','gif'];
@@ -7,7 +24,13 @@ if (!empty($_FILES)) {
     $mime = finfo_file($finfo, $_FILES['file']['tmp_name']);
     // test le mime & l'extension avec pathinfo() -- On ne veut que des fichiers PNG
     if(in_array($extension, $extension_valid) && in_array($mime, $mime_valid)){
-        move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']);
+        global $varupload;
+        $varupload = uniqid().$_FILES['file']['name'];    
+
+        move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $varupload);
+        $uploaddb->upload($varupload, $iduser);
+    } else {
+        echo 'Erreur de format';
     }
 }
 ?>
